14cb846f8ecf7387c1ed0b2fea79c598927a5a27
njeffrob <njeffrob@gmail.com>
2025-03-02 10:39:15 -0800

    some small updates

--- src/main.c
+++ src/main.c
@@ -1,25 +1,39 @@
-// The plan:
-// 1. main tool/options using C, i.e. all argument parsing and calling of functions 
-// is WITHIN THIS PROGRAM
-// 2. all functions/functionality using Lua embedded withing C
-// 3. ???? hope it works I guess
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <regex.h>
+
+struct options {
+
+};
+
+// use regex:
+// check input -> if input is real try and match it to options using regex
 
 int main(int argc, char *argv[]) {
-
-    char test_string[] = "test";
-
-    //printf("\n");
-    if (strcmp(argv[1],test_string) == 0) {
-        printf(argv[1]);
-        printf("\n");
-        printf(test_string);
-        printf("\n");
+    // if the user doesn't input anything
+    if (argc < 2) {
+        printf("Usage: xti [options] [file ...] \n"
+                "Try 'xti -h' for help on getting started. \n"
+                );
+        return 1;
     }
+    // need to make sure that the first argument is along the lines of -p/s/i/o
+    else if (argv[1] != "oop") {
+        printf("Usage: xti [options] [file ...] \n"
+            "Try 'xti -h' for help on getting started. \n"
+            );
+        return 1;
+    }
+    else{
+        char test_string[] = "test";
 
-
-	return 0;
+        if (strcmp(argv[1],test_string) == 0) {
+            printf(argv[1]);
+            printf("\n");
+            printf(test_string);
+            printf("\n");
+        }
+        return 0;
+    }
 }
