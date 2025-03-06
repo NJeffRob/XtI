7fe8e226c99fd83c0053ce643e20d876d54b9bb5
njeffrob <njeffrob@gmail.com>
2025-03-06 11:46:11 -0800

    added some oop to sh_generator; more to do yet!

--- src/sh_generator.lua
+++ src/sh_generator.lua
@@ -1,6 +1,39 @@
+-- main class that all functions inherit from; design new sh function options from this!
+sh = {}
+
+function sh:sh_generator (account, ntasks, mem_per_cpu, time, calc_type, program, io_name)
+    local sh_file = io.open(io_name .. ".sh", "w")
+
+    io.output(sh_file)
+
+    io.write(
+        "#!/bin/bash", "\n",
+        "#SBATCH --account=" .. account, "\n",
+        "#SBATCH --nodes=1", "\n",
+        "#SBATCH --ntasks=" .. ntasks, "\n",
+        "#SBATCH --mem-per-cpu=" .. mem_per_cpu .. "G", "\n",
+        "#SBATCH --time=" .. time, "\n",
+        "#SBATCH --output=%j-" .. calc_type, "\n",
+        "", "\n",
+        "module purge", "\n",
+        --"module load StdEnv/" .. stdenv .. "  gcc/" .. gcc .. "  openmpi/" .. openmpi, "\n",
+        "module load " .. program, "\n",
+        "$EBROOTORCA/orca " .. io_name .. ".inp > " .. io_name .. ".out"
+    )
+
+    io.close(sh_file)
+
+    print(".sh script generated successfully.")
+end
+
+
+-- children of the parent sh class
+orca = sh
+orca:sh_generator("test", "test", "test", "test", "test", "test", "test")
+
 -- functions to make relevant sh scripts for high performance cluster submission
 function sh_maker_orca(account, ntasks, mem_per_cpu, time, calc_type, stdenv, gcc, openmpi, orca, io_name)
-    sh_file = io.open(io_name .. ".sh", "w")
+    local sh_file = io.open(io_name .. ".sh", "w")
 
     io.output(sh_file)
 
