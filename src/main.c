ff44b68e1cfb5c9e5b7abecd55a017cab7a7f54a
njeffrob <njeffrob@gmail.com>
2025-03-08 03:33:27 -0800

    backup committing because laptop is weird

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 #include "lauxlib.h" 
 #include "lualib.h"
 
-#define GOOD 0
-#define BAD 1
+#define SUCCESS 0
+#define FAILURE 1
 
 // the following structs are for program and job options
 struct Programs {
@@ -46,9 +46,6 @@ struct Job_types {
 };
 
 
-/* use regex:
-check input -> if input is real try and match it to options using regex */
-
 // Regex; need to make sure that the first argument is along the lines of -p/s/i/o/j
 int input_option_checking(const char * user_input) {
     regex_t regex;
@@ -57,27 +54,21 @@ int input_option_checking(const char * user_input) {
 
     /* below is the best I could come up with 
     KNOWN ISSUE: if the user double inputs an option, it is accepted, e.g. -pp */
-    const char *input_options = "^-[psiojh]{1,6}";
+    const char *input_options = "^-[psioj]{1,6}";
 
     xti_input_options = regcomp(&regex, input_options, REG_EXTENDED|REG_NOSUB); 
 
     xti_input_options = regexec(&regex, user_input, 0, NULL, 0);
 
-    if (xti_input_options == 0) {
-        printf("hurray! \n");
-
+    if (xti_input_options == SUCCESS) {
         regfree(&regex);
 
-        return GOOD;
+        return SUCCESS;
     }
     else {
-        printf("Usage: xti [options] [file ...] \n"
-            "Try 'xti -h' for help on getting started. \n"
-            );
-
         regfree(&regex);
 
-        return BAD;
+        return FAILURE;
     }
 }
 
@@ -91,23 +82,32 @@ int main(int argc, char *argv[]) {
                 "Try 'xti -h' for help on getting started. \n"
                 );
         
-        return BAD;
+        return FAILURE;
     }
     else{
         /* this else section has to initialize a Lua API call, i.e. all the following code
             has to be able to run lua
-           if the user returns an appropriate input option, check the next line
-           if the next line is appropriate for the input options listed, check the next line 
-           if the next line is a name of a file in the directory, do stuff */
+           if the user returns an appropriate input option, check the next input
+           if the next input is appropriate for the input options listed, check the next input 
+           if the next input is a name of a file in the directory, do stuff */
         lua_State *L = luaL_newstate();
         luaL_openlibs(L);
 
+        // prints the help message from the lua file
+        // NOTE: LUA FILE IS NOT COMPILED WITH THE REST OF THE PROGRAM
         if (strcmp(argv[1],help) == 0) {
-            if (luaL_dofile(L, "help.lua") == LUA_OK) {
+            if (luaL_dofile(L, "src/help.lua") == LUA_OK) {
                 lua_pop(L, lua_gettop(L));
             }
+        }   
+        else if (input_option_checking(argv[1]) == SUCCESS) {
+            printf("%s\n", argv[1]);
         }
-
+        else {
+            printf("Usage: xti [options] [file ...] \n"
+                "Try 'xti -h' for help on getting started. \n"
+                );
+        }
         /*
         int i;
         for (i = 1; i < argc; i++) {
@@ -116,6 +116,6 @@ int main(int argc, char *argv[]) {
         */
         lua_close(L);
 
-        return GOOD;
+        return SUCCESS;
     }
 }
