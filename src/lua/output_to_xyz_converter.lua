85ce0e3014c2e20196843b40435795bd25c479e8
njeffrob <njeffrob@gmail.com>
2025-03-29 14:56:51 +0000

    added gamess to xyz_to_input_converter

--- src/lua/output_to_xyz_converter.lua
+++ src/lua/output_to_xyz_converter.lua
@@ -92,7 +92,77 @@ end
 
 -- gamess: in progress
 function gamess_to_xyz(io_name, calc_type)
-    
+
+    --[[
+    local gamess_output_file = assert(io.open(io_name, "r"))
+
+    local found = false
+
+    local initial_coords = {}
+    local cleanup_coords = {}
+    local xyz_coords = {}
+
+    for line in gamess_output_file:lines() do
+        if found then
+            table.insert(initial_coords, line)
+            if line == "" then
+                found = false
+            end
+        elseif line == "CARTESIAN COORDINATES (ANGSTROEM)" then
+            found = true 
+        end
+    end
+
+    gamess_output_file:close()
+
+    -- read the coordinates table backwards to get the final geometry
+    for i = #initial_coords, 1, -1 do
+        table.insert(cleanup_coords, initial_coords[i])
+    end
+
+    table.remove(cleanup_coords, 1) -- gets rid of first blank line for following loop
+
+    for k, _ in ipairs(cleanup_coords) do
+        table.insert(xyz_coords, cleanup_coords[k])
+        if cleanup_coords[k] == "" then
+            table.remove(xyz_coords, k)
+            table.remove(xyz_coords, k-1)
+            break
+        elseif string.match(cleanup_coords[k], "%-%-") then
+            table.remove(xyz_coords, k)
+            break
+        end
+    end
+
+    local number_of_atoms = 0
+
+    for k, _ in ipairs(xyz_coords) do
+        number_of_atoms = k
+    end
+
+    -- cleanup the io name to remove the original file extension
+    io_name = string.gsub(io_name, "[.][^%.]*$", "")
+
+    -- add table coordinates to new xyz file
+    local xyz_from_ouput = assert(io.open(io_name .. "-" .. calc_type .. ".xyz", "w"))
+
+    xyz_from_ouput:write(
+        number_of_atoms, "\n",
+        "XYZ file generated from XtI.", "\n"
+    )
+
+    xyz_from_ouput:close()
+
+    local xyz_from_ouput = assert(io.open(io_name .. "-" .. calc_type .. ".xyz", "a"))
+
+    for _, v in ipairs(xyz_coords) do
+        xyz_from_ouput:write(v .. "\n")
+    end
+
+    xyz_from_ouput:close()
+
+    print("XYZ coordinate file generated successfully.")
+    --]]
 end
 
 -- qe: in progress
@@ -270,5 +340,4 @@ function guassian_to_xyz(io_name, calc_type)
     print("XYZ coordinate file generated successfully.")   
 end
 
-fhiaims_to_xyz("H2O.reference.out", "opt")
-
+gamess_to_xyz("", "")
