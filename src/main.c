4a7256ac10cc395b5263cbdf2336a2faa1ddc659
asahi-nixos <Lawrence.Matsuoka@proton.me>
2025-03-28 23:11:42 -0300

    make code more readable

--- src/main.c
+++ src/main.c
@@ -1,10 +1,8 @@
 #include "lauxlib.h"
 #include "lua.h"
 #include "lualib.h"
-// #include <regex.h>
 #include <stdbool.h>
 #include <stdio.h>
-// #include <stdlib.h>
 #include <string.h>
 
 #define SUCCESS 0
@@ -24,68 +22,85 @@ struct Programs {
   char castep[7];
 };
 
+const char *valid_program[] = {"abinit",   "gamess",  "qe",    "orca",
+                               "gaussian", "fhiaims", "qchem", "siesta",
+                               "vasp",     "castep"};
+
 struct Job_types {
   char opt[30];
   char freq[30];
   char sp[30];
 };
 
-// int input_option_checking(const char *user_input);
-// int program_option_checking(const char *user_input);
-void help_message(void);
+void help_prompt(void);
 bool is_valid_option(char c);
-bool is_duplicate_option(char *str);
-void store_option(char option);
+bool is_duplicate_option(const char *str);
+void option_to_lua(char option);
 void execute_lua(const char *script);
 
 int main(int argc, char *argv[]) {
-
+  // option
   char help[3] = "-h";
-
-  // if the user doesn't input anything
+  const char *option = argv[1]; // Store argv[1] in a variable
   // Add later: limit upper bound of argc as well
   if (argc < 2) {
-    help_message();
+    help_prompt();
     return FAILURE;
   }
   // prints the help message from the lua file
   // NOTE: LUA FILE IS NOT COMPILED WITH THE REST OF THE PROGRAM
-  if (strcmp(argv[1], help) == SUCCESS) {
-    execute_lua("src/help.lua");
+  if (strcmp(option, help) == SUCCESS) {
+    execute_lua("lua/help.lua");
     return SUCCESS;
   }
   // First character of the argument is '-'
-  else if (argv[1][0] != '-') {
+  else if (option[0] != '-') {
     printf("No options specified\n"); // throw help message at the end of each
+    help_prompt();
     return FAILURE;
   }
   // Argument is between 2 to 5 characters
-  else if (strlen(argv[1]) < 2 || strlen(argv[1]) > 5) {
-    printf("Invalid number of arguments provided: %lu\n", strlen(argv[1]));
+  else if (strlen(option) < 2 || strlen(option) > 5) {
+    printf("Invalid number of arguments provided: %lu\n", strlen(option));
+    help_prompt();
     return FAILURE;
   }
   // Argument contains only the characters 's', 'i', 'o', or 'j'
-  for (int i = 1; argv[1][i] != '\0'; i++) {
-    if (!is_valid_option(argv[1][i])) {
-      printf("Invalid options '%c'\n", argv[1][i]);
+  for (int i = 1; option[i] != '\0'; i++) {
+    if (!is_valid_option(option[i])) {
+      printf("Invalid options '%c'\n", option[i]);
+      help_prompt();
       return FAILURE;
     }
   }
   // No duplicate options
-  if (is_duplicate_option(argv[1])) {
+  if (is_duplicate_option(option)) {
     printf("Duplicate options are not allowed\n");
+    help_prompt();
     return FAILURE;
   }
-
-  // Store valid options
-  for (int i = 1; argv[1][i] != '\0'; i++) {
-    store_option(argv[1][i]);
+  // Call lua API for valid options
+  for (int i = 1; option[i] != '\0'; i++) {
+    option_to_lua(option[i]);
   }
 
-  //   char *inputOptions = argv[1];
-  //    char *chemistryProgram = argv[2];
-  //    char *jobOption = argv[3];
-  //    char *file = argv[4];
+  // chemistry_program = argv[2]
+  // If valid, store and send chemistry_program to Lua
+
+  // >2, <10
+
+  // check if in the struct or whatever
+
+  // pass_argument to lua
+
+
+  // job_option = argv[3]
+  // if valid, pass argumetn to lua
+
+
+  // file = argv[4]
+
+
   // Check that chemistry_program matches a value in the struct, send to lua
   // Check that job_option matches a value in the struct, send to lua
   // Check that file is valid
@@ -106,40 +121,11 @@ int main(int argc, char *argv[]) {
   Lua.
   */
 
-  /* this else section has to initialize a Lua API call, i.e. all the
-     following code has to be able to run lua if the user returns an
-     appropriate input option, check the next input if the next input is
-     appropriate for the input options listed, check the next input if the
-     next input is a name of a file in the directory, do stuff */
-  //  lua_State *L = luaL_newstate();
-  //  luaL_openlibs(L);
-
-  // prints the help message from the lua file
-  // NOTE: LUA FILE IS NOT COMPILED WITH THE REST OF THE PROGRAM
-  /*  if (strcmp(argv[1], help) == SUCCESS) {
-      lua_State *L = luaL_newstate();
-      luaL_openlibs(L);
-      if (luaL_dofile(L, "src/help.lua") == LUA_OK) {
-        lua_pop(L, lua_gettop(L));
-      }
-      lua_close(L);
-      return SUCCESS;
-    }
-    else {
-      help_message();
-    } */
-  /*
-  int i;
-  for (i = 1; i < argc; i++) {
-      input_option_checking(argv[i]);
-  }
-  */
-  //  lua_close(L);
-
+ 
   return SUCCESS;
 }
 
-void help_message(void) {
+void help_prompt(void) {
   printf("Usage: xti [options] [file ...] \n"
          "Try 'xti -h' for help on getting started. \n");
 }
@@ -149,7 +135,7 @@ bool is_valid_option(char c) {
   return (c == 's' || c == 'i' || c == 'o' || c == 'j');
 }
 
-bool is_duplicate_option(char *str) {
+bool is_duplicate_option(const char *str) {
   // Check for duplicate characters
   for (int i = 1; str[i] != '\0'; i++) {
     for (int j = i + 1; str[j] != '\0'; j++) {
@@ -162,7 +148,9 @@ bool is_duplicate_option(char *str) {
 }
 
 // Temporarily just prints out whichever options are valid
-void store_option(char option) {
+// Add another param, char *script, then execute_lua(script) instead of
+// printing.
+void option_to_lua(char option) {
   // Call Lua based on option
   switch (option) {
   case 's':
@@ -197,36 +185,3 @@ void execute_lua(const char *script) {
 
   lua_close(L);
 }
-
-/*
-// Regex; need to make sure that the first argument is along the lines of
-// -p/s/i/o/j
-int input_option_checking(const char *user_input) {
-  regex_t regex;
-
-  // regex that Lawrence came up with that works <3
-  const char *input_options =
-      "^-(?!.*(.).*\1)[sioj]+(?: (?!.*(.).*\1)[sioj]+)*$";
-
-  // Check if input option compiles regex
-  int regexCompile = regcomp(&regex, input_options, REG_EXTENDED | REG_NOSUB);
-
-  if (regexCompile != SUCCESS) {
-    char errorBuffer[100];
-    regerror(regexCompile, &regex, errorBuffer, sizeof(errorBuffer));
-    fprintf(stderr, "Regex compile failed: %s\n", errorBuffer);
-    return FAILURE; // return FAILURE if regex compilation fails
-  }
-
-  int xti_input_options = regexec(&regex, user_input, 0, NULL, 0);
-  regfree(&regex);
-
-  if (xti_input_options == SUCCESS) {
-    return SUCCESS;
-  } else {
-    return FAILURE;
-  }
-}
-
-int program_option_checking(const char *user_input) {}
-*/
