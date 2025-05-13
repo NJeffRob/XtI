3f4fa0484e3d4ef9bf8b55b901528079c61b4ae1
Nicholas J. Roberts <njeffrob@gmail.com>
2025-05-13 15:39:22 -0300

    simple change of main.lua to the toc generator that it is

---
+++ src/lua/xti_toc.lua
@@ -0,0 +1,23 @@
+--[[ 
+XtI is a program designed to convert xyz files automatically into input
+files for a variety of quantum chemistry packages
+--]]
+
+-- ASCII character art for nice lil touch :)
+print([[
+__/\\\_______/\\\________________/\\\\\\\\\\\_        
+ _\///\\\___/\\\/________________\/////\\\///__       
+  ___\///\\\\\\/________/\\\__________\/\\\_____      
+   _____\//\\\\_______/\\\\\\\\\\\_____\/\\\_____     
+    ______\/\\\\______\////\\\////______\/\\\_____    
+     ______/\\\\\\________\/\\\__________\/\\\_____   
+      ____/\\\////\\\______\/\\\_/\\______\/\\\_____  
+       __/\\\/___\///\\\____\//\\\\\____/\\\\\\\\\\\_ 
+        _\///_______\///______\/////____\///////////__
+
+- Easy to install 
+- Available on all major operating systems
+- Straightforward to add customizable user scripts
+- Compatible with many electronic structure programs
+]])
+
